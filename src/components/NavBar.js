08f593a3ddca257ee0dc5e4de3e2f394ed419abb
Jackline Jebet <jebetjackline2016@gmail.com>
2022-07-13 09:07:00 +0300

    Done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,11 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const elements = links.map(link => {
+    return <a key={link} href={"#" + link}>{link}</a>;
+  })
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{elements}</nav>;
 }
 
 export default NavBar;
